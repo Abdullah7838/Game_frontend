77acb90e18e32f97b66fe9387fc3facad115c31d
Abdullah <abdullahluqmandudebro@gmail.com>
2024-11-11 06:34:51 +0000

    firtsyw

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -18,10 +18,10 @@ function Popup() {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed top-40 left-1/2 transform -translate-x-1/2 z-50 bg-zinc-900 w-72 h-56 rounded-lg flex flex-col items-center justify-center shadow-lg transition-transform duration-300 ease-in-out">
+        <div className="fixed top-40 left-1/2 transform -translate-x-1/2 z-50 bg-zinc-700 w-72 h-56 rounded-lg flex flex-col items-center justify-center shadow-lg transition-transform duration-300 ease-in-out">
             <div className="font-bold text-center text-white text-xl mb-2">Announcement</div>
             <div className="text-center text-blue-400 p-2">
-                Withdraws are opening on 31 October.
+                Withdraws are Open Now!.
                 Watch ads to earn cash as much as you can.
                 Share and increase the community.
             </div>
